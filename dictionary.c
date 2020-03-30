707bd72acff9a1d29ebd1baa4e142508f44c997b
Peter Zheng <scholarpetez@gmail.com>
2020-03-29 22:32:19 -0400

    Added functions that made the dictionary data structure in their own c file and header file

---
+++ dictionary.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "dictionary.h"
+
+char** make_dictionary(char* dictionary_filename){
+
+        FILE* fp = fopen(dictionary_filename, "r");
+
+        if(fp == NULL){
+                printf("File open failed for dictionary.\n");
+                exit(1);
+        }
+
+        size_t size = 0;
+        char* read_word = NULL;
+        int dictionary_size = 0;
+
+        while(getline(&read_word, &size, fp) != -1){
+                dictionary_size++;
+        }
+
+        fseek(fp, 0, SEEK_SET);
+
+        char** dictionary = (char**)malloc((dictionary_size + 1) * sizeof(char*));
+
+        int i = 0;
+
+        printf("Dict: %d\n", dictionary_size);
+
+        while(getline(&read_word, &size, fp) != -1){
+
+                dictionary[i] = (char*) malloc((strlen(read_word) + 1) * sizeof(char));
+
+                strncpy(dictionary[i], read_word, strlen(read_word) + 1);
+
+                i++;
+        }
+
+        printf("i : %d\n", i);
+
+        dictionary[i] = NULL;
+
+        free(read_word);
+
+        return dictionary;
+}
+
+void free_dictionary(char*** dictionary){
+
+        int i = 0;
+
+        while((*dictionary)[i] != NULL){
+                free((*dictionary)[i]);
+                i++;
+        }
+        free(*dictionary);
+}
+                                       
---
+++ dictionary.h
@@ -0,0 +1,7 @@
+#ifndef DICTIONARY_H
+#define DICTIONARY_H
+
+char** make_dictionary(char*);
+void free_dictionary(char***);
+
+#endif
